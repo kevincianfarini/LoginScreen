b9dbed2592e4f1f29c723b6f32a945e8b045b4b2
Kevin Cianfarini <kevincianfarini@gmail.com>
2017-02-05 21:17:04 -0500

    image views

--- app/src/main/java/com/example/kevin/loginscreen/LoginActivity.java
+++ app/src/main/java/com/example/kevin/loginscreen/LoginActivity.java
@@ -1,12 +1,38 @@
 package com.example.kevin.loginscreen;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText username;
+    private EditText password;
+    private ImageView login_button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_screen);
+        username = (EditText) findViewById(R.id.editText);
+        password = (EditText) findViewById(R.id.editText2);
+        login_button = (ImageView) findViewById(R.id.login_button);
+    }
+
+    public void checkLogin(View v) {
+        if ((this.username.getText().toString().equals("Kevin")) &&
+                (this.password.getText().toString().equals("poop"))) {
+            this.launchMain();
+        }
+    }
+
+    public void launchMain() {
+        Intent i = new Intent(this, MainActivity.class);
+        this.startActivity(i);
+        this.finish();
     }
 }
