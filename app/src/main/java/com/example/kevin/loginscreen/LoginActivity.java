4f636d0b396ff00b28ed96f197b7586a80933f31
Kevin Cianfarini <kevincianfarini@gmail.com>
2017-02-07 23:17:30 -0500

    major changes to loginscreen.xml

--- app/src/main/java/com/example/kevin/loginscreen/LoginActivity.java
+++ app/src/main/java/com/example/kevin/loginscreen/LoginActivity.java
@@ -2,31 +2,32 @@ package com.example.kevin.loginscreen;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.ImageView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
     private EditText username;
     private EditText password;
-    private ImageView login_button;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
-        username = (EditText) findViewById(R.id.editText);
-        password = (EditText) findViewById(R.id.editText2);
-        login_button = (ImageView) findViewById(R.id.login_button);
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
     }
 
     public void checkLogin(View v) {
         if ((this.username.getText().toString().equals("Kevin")) &&
                 (this.password.getText().toString().equals("poop"))) {
             this.launchMain();
+        } else {
+            Snackbar.make(findViewById(R.id.password), "Wrong username or password, dummy", Snackbar.LENGTH_LONG).show();
         }
     }
 
