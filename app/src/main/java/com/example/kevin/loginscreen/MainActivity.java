38e8339e58880ac49ef6bed24050b74ce0fccaeb
Kevin Cianfarini <kevincianfarini@gmail.com>
2017-02-10 17:44:26 -0600

    finishing requirements

--- app/src/main/java/com/example/kevin/loginscreen/MainActivity.java
+++ app/src/main/java/com/example/kevin/loginscreen/MainActivity.java
@@ -11,6 +11,8 @@ public class MainActivity extends AppCompatActivity {
     TextView my_ranking;
     ProgressBar my_steps;
     ProgressBar group_steps;
+    TextView my_steps_text;
+    TextView group_steps_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +23,16 @@ public class MainActivity extends AppCompatActivity {
         my_ranking = (TextView) findViewById(R.id.my_ranking_textview);
         my_steps = (ProgressBar) findViewById(R.id.my_steps_progressbar);
         group_steps = (ProgressBar) findViewById(R.id.group_steps_progressbar);
+        group_steps_text = (TextView) findViewById(R.id.group_steps_text);
+        my_steps_text = (TextView) findViewById(R.id.my_steps_text);
 
         my_steps.setProgress(50);
         group_steps.setProgress(37);
 
         group_ranking.setText("3/47");
         my_ranking.setText("8/512");
+        my_steps_text.setText("1,151 steps");
+        group_steps_text.setText("18,000 steps");
 
     }
 }
